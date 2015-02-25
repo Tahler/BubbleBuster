203613fbe5bf64396fbe51b59b2094eabeda88bf
Tyler Berry <tyler@berryac.com>
2015-02-25 15:09:56 -0700

    Good stopping point. Pops work.

---
+++ src/edu/neumont/csc150/bubblebuster/BubbleButton.java
@@ -0,0 +1,32 @@
+package edu.neumont.csc150.bubblebuster;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.border.EmptyBorder;
+
+public class BubbleButton extends JButton {
+	public static final ImageIcon IMG = new ImageIcon("images\\button2.png");
+	
+	public BubbleButton() {
+		this("");
+	}
+	public BubbleButton(String text) {
+		super(text);
+//		setBorder(new EmptyBorder(0, 0, 0, 0));
+//		setMaximumSize(new Dimension(IMG.getIconWidth(), IMG.getIconHeight()));
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		g.drawImage(IMG.getImage(), 0, 0, null);
+		g.setFont(new Font("SansSerif", Font.BOLD, 36));
+		
+		Rectangle2D metrics = g.getFontMetrics().getStringBounds(super.getText(), g);
+		g.drawString(super.getText(), (int) (IMG.getIconWidth() / 2 - metrics.getWidth() / 2), (int) (IMG.getIconHeight() / 2 + metrics.getHeight() / 2));
+	}
+}
