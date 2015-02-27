a4a1cce816a00ed3b9adbe8839754fb2d54bb431
Tyler Berry <tyler@berryac.com>
2015-02-27 11:12:34 -0700

    BorderLayout looks okay Would like the buttons to be less weird and long

--- src/edu/neumont/csc150/bubblebuster/GameOverPanel.java
+++ src/edu/neumont/csc150/bubblebuster/GameOverPanel.java
@@ -1,5 +1,7 @@
 package edu.neumont.csc150.bubblebuster;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
@@ -8,7 +10,8 @@ import javax.swing.JPanel;
 
 public abstract class GameOverPanel extends JPanel {
 	private GUI frame;
-	protected JLabel pointsLabel, pointsEarnedLabel, pointsRecordLabel,
+	protected JPanel tablePanel, buttonPanel;
+	protected JLabel titleLabel, col1Label, col2Label, pointsLabel, pointsEarnedLabel, pointsRecordLabel,
 		coinsLabel, coinsEarnedLabel, coinsRecordLabel,
 		currentCoinsLabel, balanceLabel;
 	private JButton playAgainButton, menuButton;
@@ -16,20 +19,42 @@ public abstract class GameOverPanel extends JPanel {
 	public GameOverPanel(GUI frame, int score) {
 		initializeComponents(score);
 		addActionListeners();
-		setLayout(new GridLayout(4, 3)); // TODO use spacing
+		setLayout(new BorderLayout(0, 0)); // TODO use spacing
 	}
 	private void initializeComponents(int score) {
-		pointsLabel = new JLabel("Points: ");
-		pointsEarnedLabel = new JLabel(score + "");
-		pointsRecordLabel = new JLabel();
+		tablePanel = new JPanel(new GridLayout(5, 3)); // TODO use spacing
+		buttonPanel = new JPanel(new GridLayout(1, 2, 0, 100));
+		tablePanel.setBackground(this.getBackground());
+		buttonPanel.setBackground(this.getBackground());
+		
+		titleLabel = new JLabel("Game Over", JLabel.CENTER);
+		titleLabel.setFont(new Font("Arial", Font.BOLD, 36));
+		
+		col1Label = new JLabel("This Run", JLabel.CENTER);
+		col2Label = new JLabel("Record", JLabel.CENTER);
+		pointsLabel = new JLabel("Points: ", JLabel.RIGHT);
+		pointsEarnedLabel = new JLabel(score + "", JLabel.CENTER);
+		pointsRecordLabel = new JLabel("", JLabel.CENTER);
 //		pointsRecordLabel = new JLabel(Statistics.survivalHighScore + "");
-		coinsLabel = new JLabel("Coins: ");
-		coinsEarnedLabel = new JLabel((score / 10) + "");
-		coinsRecordLabel = new JLabel();
+		coinsLabel = new JLabel("Coins: ", JLabel.RIGHT);
+		coinsEarnedLabel = new JLabel((score / 10) + "", JLabel.CENTER);
+		coinsRecordLabel = new JLabel("", JLabel.CENTER);
 //		coinsRecordLabel = new JLabel(Statistics.)
-		currentCoinsLabel = new JLabel("Your coins: ");
+		currentCoinsLabel = new JLabel("Your coins: ", JLabel.RIGHT);
 //		balanceLabel = new JLabel(Statistics.walletCoins + "");
-		balanceLabel = new JLabel();
+		balanceLabel = new JLabel("", JLabel.CENTER);
+		
+		Font headerFont = new Font("Arial", Font.BOLD, 24);
+		Font contentFont = new Font("Arial", Font.PLAIN, 18);
+		col1Label.setFont(headerFont);
+		col2Label.setFont(headerFont);
+		pointsLabel.setFont(headerFont);
+		coinsLabel.setFont(headerFont);
+		currentCoinsLabel.setFont(headerFont);
+		pointsEarnedLabel.setFont(contentFont);
+		pointsRecordLabel.setFont(contentFont);
+		coinsEarnedLabel.setFont(contentFont);
+		balanceLabel.setFont(contentFont);
 		
 		playAgainButton = new JButton("Play Again");
 		menuButton = new JButton("Back to Menu");
@@ -44,15 +69,22 @@ public abstract class GameOverPanel extends JPanel {
 		});
 	}
 	protected void addComponents() {
-		add(pointsLabel);
-		add(pointsEarnedLabel);
-		add(pointsRecordLabel);
-		add(coinsLabel);
-		add(coinsEarnedLabel);
-		add(coinsRecordLabel);
-		add(currentCoinsLabel);
-		add(balanceLabel);
-		add(new JPanel());
+		tablePanel.add(pointsLabel);
+		tablePanel.add(pointsEarnedLabel);
+		tablePanel.add(pointsRecordLabel);
+		tablePanel.add(coinsLabel);
+		tablePanel.add(coinsEarnedLabel);
+		tablePanel.add(coinsRecordLabel);
+		tablePanel.add(currentCoinsLabel);
+		tablePanel.add(balanceLabel);
+		tablePanel.add(new JLabel());
+		
+		buttonPanel.add(playAgainButton);
+		buttonPanel.add(menuButton);
+		
+		add(titleLabel, BorderLayout.NORTH);
+		add(tablePanel, BorderLayout.CENTER);
+		add(buttonPanel, BorderLayout.SOUTH);
 	}
 	
 	public void animateTransfer() {
--- src/edu/neumont/csc150/bubblebuster/SurvivalGameOverPanel.java
+++ src/edu/neumont/csc150/bubblebuster/SurvivalGameOverPanel.java
@@ -1,5 +1,8 @@
 package edu.neumont.csc150.bubblebuster;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -9,17 +12,30 @@ public class SurvivalGameOverPanel extends GameOverPanel {
 	public SurvivalGameOverPanel(GUI frame, int score, String time) {
 		super(frame, score);
 		// update statistics
-		timeLabel = new JLabel("Time Survived: ");
-		timeSurvivedLabel = new JLabel(time);
-		timeRecordLabel = new JLabel();
-//		timeRecordLabel = new JLabel(Statistics.survivalLongestTime);
+		initializeComponents(time);
 		addComponents();
+		
+		setBackground(new Color(0, 195, 217));
+	}
+	private void initializeComponents(String time) {
+		timeLabel = new JLabel("Time Survived: ", JLabel.RIGHT);
+		timeSurvivedLabel = new JLabel(time, JLabel.CENTER);
+		timeRecordLabel = new JLabel("", JLabel.CENTER);
+//		timeRecordLabel = new JLabel(Statistics.survivalLongestTime);
+		
+		Font headerFont = new Font("Arial", Font.BOLD, 24);
+		Font contentFont = new Font("Arial", Font.PLAIN, 18);
+		timeLabel.setFont(headerFont);
+		timeSurvivedLabel.setFont(contentFont);
+		timeRecordLabel.setFont(contentFont);
 	}
 	protected void addComponents() {
-		add(timeLabel);
-		add(timeSurvivedLabel);
-		add(timeRecordLabel);
+		tablePanel.add(new JLabel());
+		tablePanel.add(col1Label);
+		tablePanel.add(col2Label);
+		tablePanel.add(timeLabel);
+		tablePanel.add(timeSurvivedLabel);
+		tablePanel.add(timeRecordLabel);
 		super.addComponents();
 	}
-	
 }
