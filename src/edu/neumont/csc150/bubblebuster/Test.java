79d86c048f94b2a5cc0b4e39f9c256c12b4884f2
Tyler Berry <tyler@berryac.com>
2015-03-03 14:34:40 -0700

    Main method loads preferences. Removed statistics from the repo

--- src/edu/neumont/csc150/bubblebuster/Test.java
+++ src/edu/neumont/csc150/bubblebuster/Test.java
@@ -2,9 +2,10 @@ package edu.neumont.csc150.bubblebuster;
 
 import java.io.IOException;
 
-public class Test {
-
+public class Test { // TODO: remove sysout and rename to Program or Main or Driver, etc.
+	
 	public static void main(String[] args) {
+		// Load or make statistics
 		try {
 			Statistics.load();
 		}
@@ -17,8 +18,26 @@ public class Test {
 			}
 		}
 		
+		// Load or make preferences
+		try {
+			Preferences.load();
+		}
+		catch (Exception e) {
+			try {
+				Preferences.musicEnabled = true;
+				Preferences.soundEffectsEnabled = true;
+				Preferences.skinFolderLocation = "";
+				Preferences.ambianceFolderLocation = "";
+				Preferences.cursorImageFile = "";
+				Preferences.save();
+			}
+			catch (IOException e1) {
+				System.out.println("Super broken af");
+			}
+		}
+		
+		// Run the program
 		@SuppressWarnings("unused")
 		GUI gui = new GUI("Bubble Buster");
 	}
-
 }
