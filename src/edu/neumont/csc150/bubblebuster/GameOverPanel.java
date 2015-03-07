84d4008bd49ee13b8ec0e5690aa70a9146bf9a24
Tyler Berry <tyler@berryac.com>
2015-03-07 13:24:15 -0700

    Changing the gameover screen

--- src/edu/neumont/csc150/bubblebuster/GameOverPanel.java
+++ src/edu/neumont/csc150/bubblebuster/GameOverPanel.java
@@ -2,7 +2,8 @@ package edu.neumont.csc150.bubblebuster;
 
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Image;
 
 import javax.swing.GroupLayout.Alignment;
@@ -12,23 +13,20 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public abstract class GameOverPanel extends JPanel {
 	protected GUI frame;
-	protected JLabel titleLabel, col1Label, col2Label, pointsLabel, pointsEarnedLabel, pointsRecordLabel,
+	protected JLabel titleLabel, colThisRunLabel, colRecordLabel, 
+		pointsLabel, pointsEarnedLabel, pointsRecordLabel,
 		coinsLabel, coinsEarnedLabel, coinsRecordLabel,
 		currentCoinsLabel, balanceLabel;
 	protected BubbleButton playAgainButton, menuButton;
 	private Image background;
 	
-	// TODO: USE GRIDBAGLAYOUT
 	public GameOverPanel(GUI frame, Image background, int score, long time) {
 		this.frame = frame;
 		this.background = background;
 		updateStatistics(score, time);
 		
 		initializeComponents(score);
-		addActionListeners();
-		
-		GridLayout grid = new GridLayout(10, 4);
-		setLayout(grid);
+		addComponents();
 	}
 	private void updateStatistics(int score, long time) {
 		Statistics.walletCoins += score / 10;
@@ -40,10 +38,8 @@ public abstract class GameOverPanel extends JPanel {
 	}
 	private void initializeComponents(int score) {
 		titleLabel = new JLabel("Game Over", JLabel.CENTER);
-		titleLabel.setFont(new Font("Arial", Font.BOLD, 64));
-		
-		col1Label = new JLabel("This Run", JLabel.CENTER);
-		col2Label = new JLabel("Record", JLabel.CENTER);
+		colThisRunLabel = new JLabel("This Run", JLabel.CENTER);
+		colRecordLabel = new JLabel("Record", JLabel.CENTER);
 		pointsLabel = new JLabel("Points: ", JLabel.RIGHT);
 		pointsEarnedLabel = new JLabel(score + "", JLabel.CENTER);
 		pointsRecordLabel = new JLabel("", JLabel.CENTER); // will be updated in the subclass
@@ -53,10 +49,11 @@ public abstract class GameOverPanel extends JPanel {
 		currentCoinsLabel = new JLabel("Your coins: ", JLabel.RIGHT);
 		balanceLabel = new JLabel(Statistics.walletCoins + "", JLabel.CENTER);
 		
+		titleLabel.setFont(new Font("Arial", Font.BOLD, 64));
 		Font headerFont = new Font("Arial", Font.BOLD, 24);
 		Font contentFont = new Font("Arial", Font.PLAIN, 18);
-		col1Label.setFont(headerFont);
-		col2Label.setFont(headerFont);
+		colThisRunLabel.setFont(headerFont);
+		colRecordLabel.setFont(headerFont);
 		pointsLabel.setFont(headerFont);
 		coinsLabel.setFont(headerFont);
 		currentCoinsLabel.setFont(headerFont);
@@ -67,54 +64,68 @@ public abstract class GameOverPanel extends JPanel {
 		
 		playAgainButton = new BubbleButton("Play Again");
 		menuButton = new BubbleButton("Back to Menu");
-	}
-	protected void addActionListeners() {
 		menuButton.addActionListener(e -> {
 			frame.switchTo(new MainMenuPanel(frame));
 		});
 	}
 	
 	// Not called by the constructor, must be called from child classes
-	protected void addEmptyRow() {
-		add(new JLabel());
-		add(new JLabel());
-		add(new JLabel());
-		add(new JLabel());
-	}
 	protected void addComponents() {
-		add(pointsLabel);
-		add(pointsEarnedLabel);
-		add(pointsRecordLabel);
-		add(new JLabel());
+		setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.anchor = GridBagConstraints.CENTER;
 		
-		add(coinsLabel);
-		add(coinsEarnedLabel);
-		add(coinsRecordLabel);
-		add(new JLabel());
+		c.gridwidth = 3;
+		c.gridx = 0;
+		c.gridy = 0;
+		add(titleLabel, c);
 		
-		add(currentCoinsLabel);
-		add(balanceLabel);
-		add(new JLabel());
-		add(new JLabel());
+		c.gridwidth = 1;
+		c.gridx = 1;
+		c.gridy = 1;
+		add(colThisRunLabel, c);
 		
-		addEmptyRow();
+		c.gridx = 2;
+		add(colRecordLabel, c);
 		
-		add(new JLabel());
-		add(playAgainButton, Alignment.CENTER);
-		add(menuButton, Alignment.CENTER);
-		add(new JLabel());
+		c.gridx = 0;
+		c.gridy = 3; // Skipping gridy = 2, that will be filled in by the subclasses
+		add(pointsLabel,c );
 		
-		addEmptyRow();
+		c.gridx = 1;
+		add(pointsEarnedLabel, c);
+		
+		c.gridx = 2;
+		add(pointsRecordLabel, c);
+		
+		c.gridx = 0;
+		c.gridy = 3;
+		add(coinsLabel, c);
+		
+		c.gridx = 1;
+		add(coinsEarnedLabel, c);
+		
+		c.gridx = 2;
+		add(coinsRecordLabel, c);
+		
+//		add(currentCoinsLabel);
+//		add(balanceLabel);
+//		add(new JLabel());
+//		add(new JLabel());
+//		
+//		add(new JLabel());
+//		add(playAgainButton, Alignment.CENTER);
+//		add(menuButton, Alignment.CENTER);
+//		add(new JLabel());
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.drawImage(background, 0, 0, null);
-		g.setFont(new Font("Arial", Font.BOLD, 36));
-		g.drawString("Game Over", GUI.WIDTH / 2 - (int) g.getFontMetrics().getStringBounds("Game Over", g).getWidth() / 2, GUI.HEIGHT / 5);
 	}
 	
+	// TODO: animate the coins transferring
 	public void animateTransfer() {
 		
 	}
--- src/edu/neumont/csc150/bubblebuster/SurvivalGameOverPanel.java
+++ src/edu/neumont/csc150/bubblebuster/SurvivalGameOverPanel.java
@@ -8,13 +8,14 @@ import javax.swing.JLabel;
 
 @SuppressWarnings("serial")
 public class SurvivalGameOverPanel extends GameOverPanel {
-	private JLabel timeLabel, timeSurvivedLabel, timeRecordLabel;
+//	private JLabel timeLabel, timeSurvivedLabel, timeRecordLabel;
 	
 	public SurvivalGameOverPanel(GUI frame, Image background, int score, long time) {
 		super(frame, background, score, time);
 		updateStatistics(score, time);
-		initializeComponents(time);
-		addComponents();
+//		initializeComponents(time);
+		pointsRecordLabel.setText(Statistics.survivalHighScore + "");
+//		addComponents();
 	}
 	private void updateStatistics(int score, long time) {
 		if (Statistics.survivalLongestTime != null) {
@@ -33,38 +34,35 @@ public class SurvivalGameOverPanel extends GameOverPanel {
 			e.printStackTrace();
 		}
 	}
-	private void initializeComponents(long time) {
-		pointsRecordLabel.setText(Statistics.survivalHighScore + "");
-		
-		timeLabel = new JLabel("Time Survived: ", JLabel.RIGHT);
-		timeSurvivedLabel = new JLabel(Time.format(time), JLabel.CENTER);
-		if (Statistics.survivalLongestTime != null) timeRecordLabel = new JLabel(Statistics.survivalLongestTime.toString(), JLabel.CENTER);
-		else timeRecordLabel = new JLabel("", JLabel.CENTER);
-		
-		Font headerFont = new Font("Arial", Font.BOLD, 24);
-		Font contentFont = new Font("Arial", Font.PLAIN, 18);
-		timeLabel.setFont(headerFont);
-		timeSurvivedLabel.setFont(contentFont);
-		timeRecordLabel.setFont(contentFont);
-	}
-	protected void addComponents() {
-		addEmptyRow();
-		addEmptyRow();
-		add(new JLabel());
-		add(col1Label);
-		add(col2Label);
-		add(new JLabel());
-		add(timeLabel);
-		add(timeSurvivedLabel);
-		add(timeRecordLabel);
-		add(new JLabel());
-		super.addComponents();
-	}
-	protected void addActionListeners() {
-		super.addActionListeners();
-		playAgainButton.addActionListener(e -> {
-			frame.gamePanel = new SurvivalMode(frame);
-			frame.switchTo(frame.gamePanel);
-		});
-	}
+//	private void initializeComponents(long time) {
+//		pointsRecordLabel.setText(Statistics.survivalHighScore + "");
+//		
+//		timeLabel = new JLabel("Time Survived: ", JLabel.RIGHT);
+//		timeSurvivedLabel = new JLabel(Time.format(time), JLabel.CENTER);
+//		if (Statistics.survivalLongestTime != null) timeRecordLabel = new JLabel(Statistics.survivalLongestTime.toString(), JLabel.CENTER);
+//		else timeRecordLabel = new JLabel("", JLabel.CENTER);
+//		
+//		Font headerFont = new Font("Arial", Font.BOLD, 24);
+//		Font contentFont = new Font("Arial", Font.PLAIN, 18);
+//		timeLabel.setFont(headerFont);
+//		timeSurvivedLabel.setFont(contentFont);
+//		timeRecordLabel.setFont(contentFont);
+//	}
+//	protected void addComponents() {
+//		add(new JLabel());
+//		add(colThisRunLabel);
+//		add(colRecordLabel);
+//		add(new JLabel());
+//		add(timeLabel);
+//		add(timeSurvivedLabel);
+//		add(timeRecordLabel);
+//		add(new JLabel());
+//		super.addComponents();
+//	}
+//	protected void addActionListeners() {
+//		playAgainButton.addActionListener(e -> {
+//			frame.gamePanel = new SurvivalMode(frame);
+//			frame.switchTo(frame.gamePanel);
+//		});
+//	}
 }
