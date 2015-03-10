e307e2f60444a22ed716f001f743c4ed5bf58497
Tyler Berry <tyler@berryac.com>
2015-03-10 12:24:11 -0600

    Working on shop items adding preview images

--- src/edu/neumont/csc150/bubblebuster/ShopItem.java
+++ src/edu/neumont/csc150/bubblebuster/ShopItem.java
@@ -12,18 +12,18 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class ShopItem extends JPanel {
-	private JLabel title;
+	private String title;
 	private ImageIcon image;
 	private BubbleButton purchaseButton;
 	private int cost;
 	
 	public ShopItem(String title, String folderPath, int cost) {
-		this.title = new JLabel(title);
+		this.title = title;
 		// Scaled to 480x300
-		this.image = new ImageIcon(Toolkit.getDefaultToolkit().getImage(folderPath + "/background.jpg").getScaledInstance(480, 300, Image.SCALE_DEFAULT));
+		this.image = new ImageIcon(Toolkit.getDefaultToolkit().getImage(folderPath + "/preview.png").getScaledInstance(480, 300, Image.SCALE_DEFAULT));
 		this.cost = cost;
 		this.purchaseButton = new BubbleButton(cost + " Coins");
-		
+		setOpaque(false);
 		addComponents();
 	}
 	private void addComponents() {
@@ -34,22 +34,22 @@ public class ShopItem extends JPanel {
 		c.insets = new Insets(0, 0, 0, 0);
 		c.gridx = 0;
 		c.gridy = 0;
-		add(title, c);
+		add(new JLabel(title), c);
 		
 		c.insets = new Insets(0, 0, 0, 0);
-		c.gridx = 1;
-		c.gridy = 0;
+		c.gridx = 0;
+		c.gridy = 1;
 		add(new JLabel(image), c);
 		
 		c.insets = new Insets(0, 0, 0, 0);
-		c.gridx = 2;
-		c.gridy = 0;
-		add(purchaseButton, c);
+		c.gridx = 0;
+		c.gridy = 2;
+		if (!Purchases.isPurchased(title)) add(purchaseButton, c);
 	}
 	
 	public void purchase() {
 		if (Statistics.walletCoins >= cost) {
-			Purchases.purchaseAndUse(title.getText());
+			Purchases.purchaseAndUse(title);
 			
 			Statistics.walletCoins -= cost;
 			purchaseButton.setVisible(false);
